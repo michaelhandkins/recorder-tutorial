9d3218c9e75de85a3c44abe8644f89aa3396b7c6
Michael Handkins <michaelhandkins@gmail.com>
2020-09-25 20:50:03 -0500

    Initial commit

---
+++ Voice Recorder (YT Tutorial)/Controller/RecorderViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Voice Recorder (YT Tutorial)
+//
+//  Created by Michael Handkins on 9/25/20.
+//
+
+import UIKit
+
+class RecorderViewController: UIViewController {
+
+    @IBAction func recordPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func playPressed(_ sender: UIButton) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Voice Recorder (YT Tutorial)Tests/Voice_Recorder__YT_Tutorial_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Voice_Recorder__YT_Tutorial_Tests.swift
+//  Voice Recorder (YT Tutorial)Tests
+//
+//  Created by Michael Handkins on 9/25/20.
+//
+
+import XCTest
+@testable import Voice_Recorder__YT_Tutorial_
+
+class Voice_Recorder__YT_Tutorial_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
